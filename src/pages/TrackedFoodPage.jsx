370efaa869bf9a1e868281fb385d789505aea1c3
Hans Koh <hansdarrelkoh@gmail.com>
2026-03-23 10:09:27 +0800

    tracked food page now tracks food names. to do: macronutrients

--- src/pages/TrackedFoodPage.jsx
+++ src/pages/TrackedFoodPage.jsx
@@ -8,14 +8,27 @@ const TrackedFoodPage = ({ trackedFoods }) => {
 
                 {trackedFoods.length > 0 ? (
                     //! replace this portion with js for the trackedFoods.map
-                    <p>Tracked Foods List Placeholder</p>
+                    <ul>
+                        {
+                            trackedFoods.map((foods) => (
+                                <li>{foods.product_name}</li>
+                            ))
+                        }
+                    </ul>
                 ) : (
                     <p>No foods tracked. Click "Add" to begin logging.</p>
                 )}
 
             </div>
 
-
+            {/* for reference only
+            
+            foods.length > 0 ? (foods.map((food) => (
+                        <FoodCard key={food._id} item={food} onAdd={handleLastAdded}/>))
+                    ) : (
+                        <p>Search for a food to begin.</p>
+                    ) 
+                     */}
 
         </div>
     )
