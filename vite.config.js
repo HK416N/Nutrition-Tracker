e6b533448a3ede98469d01f58b5c255648a1b467
Hans Koh <hansdarrelkoh@gmail.com>
2026-03-24 20:49:15 +0800

    fix: index.html for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensures all assets are loaded from the root
+  build: {
+    outDir: 'dist', // Explicitly names the output folder
+  }
 })
